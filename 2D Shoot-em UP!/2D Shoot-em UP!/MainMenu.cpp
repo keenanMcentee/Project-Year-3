3284f0ae6e3d15e13cbcd9488f79fe0c705ff236
keenanmcentee <c00201892@itcarlow.ie>
2017-11-21 01:48:26 +0000

    New assets added for our game

--- 2D Shoot-em UP!/2D Shoot-em UP!/MainMenu.cpp
+++ 2D Shoot-em UP!/2D Shoot-em UP!/MainMenu.cpp
@@ -4,6 +4,7 @@
 
 MainMenu::MainMenu(sf::RenderWindow *window) : Screen(window)
 {
+	this->m_window = window;
 }
 
 void MainMenu::Initialise(GameState *state)
@@ -15,9 +16,9 @@ void MainMenu::Initialise(GameState *state)
 	auto button = uiHelper::makeButton("Play", sf::Vector2f(500, 300), 300, 100);
 	button->connect("pressed", [&]() {GoToScreen(GameState::Play); });
 	gui.add(button, "Menu_playBtn");
-
+	
 	button = uiHelper::makeButton("Quit", sf::Vector2f(500, 500), 300, 100);
-	button->connect("pressed", [&]() {GoToScreen(GameState::QuitScreen); });
+	button->connect("pressed", [&]() { MainMenu::m_window->close(); });
 	gui.add(button, "Menu_quitBtn");
 
 }
--- 2D Shoot-em UP!/2D Shoot-em UP!/MainMenu.h
+++ 2D Shoot-em UP!/2D Shoot-em UP!/MainMenu.h
@@ -7,8 +7,9 @@ public:
 	MainMenu(sf::RenderWindow *window);
 	void Initialise(GameState *state);
 	void Update();
+	static sf::RenderWindow* m_window;
 	void Draw(sf::RenderWindow *window);
 private:
-
+	
 };
 
